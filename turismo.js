211058ede6ca9fef3a78850daa7e83a47e276275
A01197185 <A01197185@itesm.mx>
2022-04-01 14:35:11 -0600

    VueJS 2.0 a 3.0
    Se hizo el cambio de la version de VueJS de la version 2.0 a la version 3.0. Ademas se hicieron cambios en el turismo.js para generar los cambios requeridos para que el codigo funcione con la nueva version.

--- turismo.js
+++ turismo.js
@@ -1,39 +1,44 @@
-var p2 = new Vue({ 
-    el: '#p5',
-    data: {
-        nA: false,
-        europa: false,
-        asia: false,
-        oceania: false,
-        r1: 'Norte America',
-        r2: 'Europa',
-        r3: 'Asia',
-        r4: 'Oceania',
-        bucl1:[
-            { paises:"Canada" },
-            { paises:"Estados Unidos" },
-            { paises:'Mexico' },
-        ],
-        bucl2:[
-            { paises:"Alemania" },
-            { paises:"Francia" },
-            { paises:'Portugal' },
-        ],
-        bucl3:[
-            { paises:"China" },
-            { paises:"Korea" },
-            { paises:'Japon' },
-        ],
-        bucl4:[
-            { paises:"Australia" },
-            { paises:"Nueva Zelanda" },
-            { paises:'Fiji' },
-        ]
+const p5 = Vue.createApp({ 
+    data() {
+        return{
+            nA: false,
+            europa: false,
+            asia: false,
+            oceania: false,
+            r1: 'Norte America',
+            r2: 'Europa',
+            r3: 'Asia',
+            r4: 'Oceania',
+            bucl1:[
+                { paises:"Canada" },
+                { paises:"Estados Unidos" },
+                { paises:'Mexico' },
+            ],
+            bucl2:[
+                { paises:"Alemania" },
+                { paises:"Francia" },
+                { paises:'Portugal' },
+            ],
+            bucl3:[
+                { paises:"China" },
+                { paises:"Korea" },
+                { paises:'Japon' },
+            ],
+            bucl4:[
+                { paises:"Australia" },
+                { paises:"Nueva Zelanda" },
+                { paises:'Fiji' },
+            ]
+        }
     }
 });
-var inputs = new Vue({
-    el:'#inputs',
-    data:{
-        message: ''
+p5.mount('#p5')
+
+const inputs = Vue.createApp({
+    data(){
+        return{
+        message: ''   
+        }
     }
  });
+ inputs.mount('#inputs')
