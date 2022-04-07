21920e6fb2c266c459156ba5214850e57d63c682
A01197185 <A01197185@itesm.mx>
2022-04-07 18:07:30 -0500

    Ejercicio 6
    Se agrego el uso de computed, methods y dom a la pagina

--- turismo.js
+++ turismo.js
@@ -2,7 +2,7 @@ const p5 = Vue.createApp({
     data() {
         return{
             nA: false,
-            europa: false,
+            europe: false,
             asia: false,
             oceania: false,
             r1: 'North America',
@@ -18,25 +18,60 @@ const p5 = Vue.createApp({
                 { paises:"Germany" },
                 { paises:"France" },
                 { paises:'Portugal' },
+                { paises:"Poland" },
+                { paises:"England" },
+                { paises:"Italy" },
+                { paises:"Sweden" },
+                { paises:"Norway" },
+                { paises:"Malta" },
             ],
             bucl3:[
                 { paises:"China" },
                 { paises:"Korea" },
                 { paises:'Japan' },
+                { paises:"Malaysia" },
+                { paises:"Phillipines" },
+                { paises:"India" },
+                { paises:"Khazakstan" },
+                { paises:"Indonesia" },
             ],
             bucl4:[
                 { paises:"Australia" },
                 { paises:"New Zealand" },
                 { paises:'Fiji' },
+                { paises:"Papua New Guinea" },
+                { paises:"New Caledonia" },
             ]
         }
     },
     methods: {
-        rnA(){
-            nA = !nA
+        rNA(){
+            this.nA = !this.nA
+        },
+        rEU(){
+            this.europe = !this.europe
+        },
+        rAS(){
+            this.asia = !this.asia
+        },
+        rOC(){
+            this.oceania = !this.oceania
+        }
+    },
+    computed: {
+        cntNA(){
+            return this.bucl1.length > 0 ? this.bucl1.length : ''
+        },
+        cntEU(){
+            return this.bucl2.length > 0 ? this.bucl2.length : ''
+        },
+        cntAS(){
+            return this.bucl3.length > 0 ? this.bucl3.length : ''
+        },
+        cntOC(){
+            return this.bucl4.length > 0 ? this.bucl4.length : ''
         }
     }
-
 });
 p5.mount('#p5')
 
